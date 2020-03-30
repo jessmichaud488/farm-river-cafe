18724a75c2da08e2a9602f7f33f75f4a2ddbe387
J M <jessmichaud488@gmail.com>
2020-03-30 15:43:47 -0400

    Styling of menu component

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,11 +12,15 @@ class Menu extends React.Component {
                 <p id="menu-text">Photo to be updated to a current menu.</p>
 
                 <div id="desserts-div">
-                    <p>After breakfast or lunch, make sure you take one (or two, or three) of our yummy desserts to go,</p>
-                    <p>made with love by this lady right here, the awesome Ms.Stephanie!</p>
-                    <img src={require('../images/stephanie.jpg')} alt="Stephanie" className="dessert-pic"></img>
-                    <p>With slices of crumble, bars, cupcakes, muffins, and cookies, many of which are gluten-free and vegan,</p>
-                    <p>you can always find something to satisfy a sweet tooth!</p>
+                    <p className="font-bold">After breakfast or lunch, make sure you take one (or two, or three) of our yummy desserts to go,</p>
+                    <p className="font-bold">made with love by this lady right here, the awesome Ms.Stephanie!</p>
+                    <img src={require('../images/stephanie.jpg')} alt="Stephanie" id="stephanie-pic"></img>
+                    <p className="font-bold">With slices of crumble, bars, cupcakes, muffins, and cookies, many of which are gluten-free and vegan,</p>
+                    <p className="font-bold">you can always find something to satisfy a sweet tooth!</p>
+                    <p className="font-bold">**If you have a dietary allergy/preference, please be sure to ask first!**</p>
+                </div>
+
+                <div id="desserts-images-div">
                     <img src={require('../images/barwithmug.jpg')} alt="Dessert bar with mug" className="dessert-pic"></img>
                     <img src={require('../images/cupcakes.jpg')} alt="Cupcakes" className="dessert-pic"></img>
                     <img src={require('../images/crumble.jpg')} alt="Different crumbles on display" className="dessert-pic"></img>
