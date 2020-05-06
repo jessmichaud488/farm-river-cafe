ab39d69ac50d09c366662e2496b641e2187ce75c
J M <jessmichaud488@gmail.com>
2020-05-06 19:54:06 -0400

    Added slideshow

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-export default () => (
-    <Carousel autoPlay showThumbs={false} style={{ height: "200px", width: "300px"}}>
-        <div style={{ height: "200px", width: "300px"}}>
-            <img src={require('../images/barwithmug.jpg')} alt="Dessert bar with mug"></img>
-        </div>
-        <div style={{ height: "200px", width: "300px"}}>
-            <img src={require('../images/cupcakes.jpg')} alt="Cupcakes"></img>
-        </div>
-        <div style={{ height: "200px", width: "300px"}}>
-            <img src={require('../images/crumble.jpg')} alt="Different crumbles on display"></img>
-        </div>
-        <div style={{ height: "200px", width: "300px"}}>
-            <img src={require('../images/cupcake-meditation.jpg')} alt="Cupcake with meditation cards"></img>
-        </div>
-        <div style={{ height: "200px", width: "300px"}}>
-            <img src={require('../images/desserts-case.jpg')} alt="Desserts case"></img>
-        </div>
-    </Carousel>
-);
+let styles = {
+    margin: 'auto',
+    width: '500px',
+    objectfit: 'cover'
+  };
+  
+function App() {
+  return (
+	<div style={styles}>
+		<Carousel autoPlay showThumbs={false}>
+			<div>
+				<img src={require('../images/barwithmug.jpg')} alt="Dessert bar with mug" />
+			</div>
+			<div>
+				<img src={require('../images/orangecupcake.jpg')} alt="Cupcakes" />
+			</div>
+			<div>
+				<img src={require('../images/crumble.jpg')} alt="Different crumbles on display" />
+			</div>
+			<div>
+				<img src={require('../images/cupcake-meditation.jpg')} alt="Cupcake with meditation cards" />
+			</div>
+		</Carousel>
+	</div>
+  );
+}
+
+export default App;
