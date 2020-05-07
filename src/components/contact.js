024f545c20e25fb5ed8bac38e0701afb8917ca71
J M <jessmichaud488@gmail.com>
2020-05-07 13:17:06 -0400

    Added Formspree form to Contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Form from './form'
 import MapContainer from './mapcontainer'
 
 class Contact extends React.Component {
@@ -8,40 +9,8 @@ class Contact extends React.Component {
                 <div className="logo-div">
                     <img src={require("../images/farmriverlogo-transparent.png")} alt="Farm River Cafe logo" id="farmriver-logo"></img>
                 </div>
-                
                 <MapContainer />
-                <form className="contact-form">
-                <div className="row">
-                    <div className = "column">
-                        <input
-                            className="input-block"
-                            type="text"
-                            placeholder="Your name"
-                        />
-                        <input
-                            className="input-block"
-                            type="text"
-                            placeholder="Your email"
-                            required
-                        />
-                        <input
-                            className="input-block"
-                            type="text"
-                            placeholder="Subject"
-                        />
-                    </div>
-
-                    <div className = "column">
-                        <input 
-                            id="message-box"
-                            type="text"
-                            placeholder="Drop us a line!"
-                            required
-                        />
-                    </div>
-                </div>
-                </form>
-                <button id="form-button" type="submit">SEND</button>
+                <Form />
             </div>
         )
     }
